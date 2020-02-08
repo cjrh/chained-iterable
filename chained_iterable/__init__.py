290c9c63d3e6ef31f4e7dddafb8674b3eb1e32e4
Derek Wan <d.wan@icloud.com>
2020-02-09 00:32:36 +0800

    Update PyPI description

--- chained_iterable/__init__.py
+++ chained_iterable/__init__.py
@@ -1,8 +1,8 @@
-"""The functional-itertools package."""
+"""Python iterables in a functional-programming style."""
 from chained_iterable.chained_iterable import ChainedIterable
 from chained_iterable.errors import EmptyIterableError
 from chained_iterable.errors import MultipleElementsError
 
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 _ = {EmptyIterableError, MultipleElementsError, ChainedIterable}
